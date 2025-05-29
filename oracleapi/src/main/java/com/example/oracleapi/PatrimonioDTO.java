f9639438da612ce4cad993e0ce765ee04b9ff996
Beatriz Ferreira dos Santos <BeatrizFerreirados.Santos@philips.com>
2025-05-29 15:56:28 -0300

    Initial commit

---
+++ oracleapi/src/main/java/com/example/oracleapi/PatrimonioDTO.java
@@ -0,0 +1,15 @@
+package com.example.oracleapi;
+
+import java.time.LocalDate;
+import java.util.Date;
+
+public class PatrimonioDTO{
+    public Integer id;
+    public String nomePatrimonio;
+    public String codigoBarras;
+    public String setor;
+    public String marca;
+    public Date dataFabricacao;
+    public LocalDate ultimaManutencao;
+
+}
